4da3f4248911a92c8b7770730fcb23aac29fe014
Tim Jentzsch <commits@timjen.net>
2023-12-03 19:36:13 +0100

    Add day 03 boilerplate

---
+++ crates/day_03/src/main.rs
@@ -0,0 +1,19 @@
+use aoc_utils::AocDay;
+
+fn main() {
+    Day03::run();
+}
+
+struct Day03;
+
+impl AocDay<(), ()> for Day03 {
+    const DAY: u8 = 3;
+
+    fn part_1(_input: &str) {
+        todo!()
+    }
+
+    fn part_2(_input: &str) {
+        todo!()
+    }
+}
