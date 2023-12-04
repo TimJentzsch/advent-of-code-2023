117ef03b5fa82ad25c228ae3b2e87cb55942ab8c
Tim Jentzsch <commits@timjen.net>
2023-12-04 16:13:25 +0100

    Implement day 04 part 2

--- crates/day_04/src/main.rs
+++ crates/day_04/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
 struct Day04;
 
-impl AocDay<u32, ()> for Day04 {
+impl AocDay<u32, u32> for Day04 {
     const DAY: u8 = 4;
 
     fn part_1(input: &str) -> u32 {
@@ -21,10 +21,10 @@ impl AocDay<u32, ()> for Day04 {
                 let drawn_winning = drawn_numbers
                     .iter()
                     .filter(|num| winning_numbers.contains(num))
-                    .collect::<Vec<_>>();
+                    .count();
 
                 let mut score = 0;
-                for _ in drawn_winning {
+                for _ in 0..drawn_winning {
                     score = match score {
                         0 => 1,
                         _ => score * 2,
@@ -35,8 +35,38 @@ impl AocDay<u32, ()> for Day04 {
             .sum()
     }
 
-    fn part_2(_input: &str) {
-        todo!()
+    fn part_2(input: &str) -> u32 {
+        let winning_draws: Vec<_> = input
+            .trim()
+            .lines()
+            .map(|line| {
+                let (_, second) = line.split_once(':').expect("No : contained in line");
+                let (winning, draw) = second.split_once('|').expect("No | contained in line");
+                let winning_numbers = parse_number_list(winning);
+                let drawn_numbers = parse_number_list(draw);
+
+                drawn_numbers
+                    .iter()
+                    .filter(|num| winning_numbers.contains(num))
+                    .count()
+            })
+            .collect();
+
+        let mut copies: Vec<_> = winning_draws.iter().map(|_| 1).collect();
+
+        for (i, winning) in winning_draws.iter().enumerate() {
+            let card_copies = copies[i];
+
+            for j in 0..*winning {
+                let index = i + j + 1;
+
+                if index < copies.len() {
+                    copies[index] += card_copies;
+                }
+            }
+        }
+
+        copies.iter().sum()
     }
 }
 
@@ -61,5 +91,17 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
         assert_eq!(Day04::part_1(input), 13);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        assert_eq!(Day04::part_2(input), 30);
     }
 }
