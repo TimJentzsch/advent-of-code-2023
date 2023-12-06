17307d909a90beb904f32ed4912a5da880021b74
Tim Jentzsch <commits@timjen.net>
2023-12-06 12:48:38 +0100

    Implement day 06 part 2

--- crates/day_06/src/main.rs
+++ crates/day_06/src/main.rs
@@ -8,33 +8,33 @@ fn main() {
 
 struct Day06;
 
-impl AocDay<usize, ()> for Day06 {
+impl AocDay<usize, usize> for Day06 {
     const DAY: u8 = 6;
 
     fn part_1(input: &str) -> usize {
-        parse_input(input)
+        parse_input_part_1(input)
             .iter()
             .map(Race::count_winning_presses)
             .product()
     }
 
-    fn part_2(_input: &str) {
-        todo!()
+    fn part_2(input: &str) -> usize {
+        parse_input_part_2(input).count_winning_presses()
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Race {
-    time: u32,
-    record: u32,
+    time: u64,
+    record: u64,
 }
 
 impl Race {
-    pub fn new(time: u32, record: u32) -> Self {
+    pub fn new(time: u64, record: u64) -> Self {
         Self { time, record }
     }
 
-    pub fn distance_with_press(&self, duration: u32) -> u32 {
+    pub fn distance_with_press(&self, duration: u64) -> u64 {
         let speed = duration;
         let time_to_move = self.time.saturating_sub(duration);
         time_to_move * speed
@@ -47,21 +47,39 @@ impl Race {
     }
 }
 
-fn parse_input(input: &str) -> Vec<Race> {
+fn parse_input_part_1(input: &str) -> Vec<Race> {
     let (time_line, distance_line) = input.trim().split_once('\n').unwrap();
 
-    zip(parse_line(time_line), parse_line(distance_line))
-        .map(|(time, distance)| Race::new(time, distance))
-        .collect()
+    zip(
+        parse_line_part_1(time_line),
+        parse_line_part_1(distance_line),
+    )
+    .map(|(time, distance)| Race::new(time, distance))
+    .collect()
 }
 
-fn parse_line(line: &str) -> Vec<u32> {
+fn parse_line_part_1(line: &str) -> Vec<u64> {
     let (_, raw_nums) = line.split_once(':').unwrap();
     raw_nums
         .trim()
         .split_ascii_whitespace()
-        .map(|num_str| num_str.parse::<u32>().unwrap())
+        .map(|num_str| num_str.parse::<u64>().unwrap())
         .collect()
+}
+
+fn parse_input_part_2(input: &str) -> Race {
+    let (time_line, distance_line) = input.trim().split_once('\n').unwrap();
+
+    Race {
+        time: parse_line_part_2(time_line),
+        record: parse_line_part_2(distance_line),
+    }
+}
+
+fn parse_line_part_2(line: &str) -> u64 {
+    let (_, raw_nums) = line.split_once(':').unwrap();
+    let parts: Vec<_> = raw_nums.trim().split_ascii_whitespace().collect();
+    parts.join("").parse().unwrap()
 }
 
 #[cfg(test)]
@@ -72,15 +90,25 @@ mod tests {
 Distance:  9  40  200";
 
     #[test]
-    fn test_parse_input() {
+    fn test_parse_input_part_1() {
         assert_eq!(
-            parse_input(INPUT),
+            parse_input_part_1(INPUT),
             vec![Race::new(7, 9), Race::new(15, 40), Race::new(30, 200)]
         )
     }
 
     #[test]
+    fn test_parse_input_part_2() {
+        assert_eq!(parse_input_part_2(INPUT), Race::new(71530, 940200))
+    }
+
+    #[test]
     fn test_part_1() {
         assert_eq!(Day06::part_1(INPUT), 288);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(Day06::part_2(INPUT), 71503);
     }
 }
