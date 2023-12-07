a54282f03df613bba5ccfeca5a33ff2117731194
Tim Jentzsch <commits@timjen.net>
2023-12-07 23:09:41 +0100

    Implement day 07 part 2

--- crates/day_07/src/main.rs
+++ crates/day_07/src/main.rs
@@ -9,29 +9,33 @@ fn main() {
 
 struct Day07;
 
-impl AocDay<u32, ()> for Day07 {
+impl AocDay<u32, u32> for Day07 {
     const DAY: u8 = 7;
 
     fn part_1(input: &str) -> u32 {
-        let mut list: Vec<_> = input
-            .lines()
-            .map(|line| {
-                let (hand_str, bid_str) = line.split_once(' ').unwrap();
-                Player::new(hand_str, bid_str.parse().unwrap())
-            })
-            .collect();
-
-        list.sort_unstable();
-
-        list.iter()
-            .enumerate()
-            .map(|(idx, player)| (idx as u32 + 1) * player.bid)
-            .sum()
+        total_winnings(input, false)
     }
 
-    fn part_2(_input: &str) {
-        todo!()
+    fn part_2(input: &str) -> u32 {
+        total_winnings(input, true)
     }
+}
+
+fn total_winnings(input: &str, part_2: bool) -> u32 {
+    let mut list: Vec<_> = input
+        .lines()
+        .map(|line| {
+            let (hand_str, bid_str) = line.split_once(' ').unwrap();
+            Player::new(hand_str, bid_str.parse().unwrap(), part_2)
+        })
+        .collect();
+
+    list.sort_unstable();
+
+    list.iter()
+        .enumerate()
+        .map(|(idx, player)| (idx as u32 + 1) * player.bid)
+        .sum()
 }
 
 #[cfg(test)]
@@ -47,5 +51,10 @@ QQQJA 483";
     #[test]
     fn test_part_1() {
         assert_eq!(Day07::part_1(INPUT), 6440);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(Day07::part_2(INPUT), 5905);
     }
 }
--- crates/day_07/src/types.rs
+++ crates/day_07/src/types.rs
@@ -10,9 +10,12 @@ pub struct Player<'h> {
 }
 
 impl<'h> Player<'h> {
-    pub fn new(hand_str: &'h str, bid: u32) -> Self {
+    pub fn new(hand_str: &'h str, bid: u32, part_2: bool) -> Self {
         Self {
-            hand: Hand { cards: hand_str },
+            hand: Hand {
+                cards: hand_str,
+                part_2,
+            },
             bid,
         }
     }
@@ -38,9 +41,7 @@ impl PartialEq for Player<'_> {
 
 impl Eq for Player<'_> {}
 
-const CARDS: [char; 13] = [
-    '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
-];
+const CARDS: [char; 12] = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 #[repr(u8)]
@@ -69,12 +70,13 @@ impl PartialOrd for HandType {
 #[derive(Debug, Clone)]
 struct Hand<'h> {
     cards: &'h str,
+    part_2: bool,
 }
 
 impl<'h> Hand<'h> {
     #[cfg(test)]
-    pub fn new(cards: &'h str) -> Self {
-        Self { cards }
+    pub fn new(cards: &'h str, part_2: bool) -> Self {
+        Self { cards, part_2 }
     }
 }
 
@@ -86,28 +88,41 @@ impl Hand<'_> {
                 let count = self.cards.chars().filter(|c| c == card).count();
 
                 if count > 0 {
-                    Some((card, count))
+                    Some(count)
                 } else {
                     None
                 }
             })
             .collect();
 
+        let jokers = self.cards.chars().filter(|c| *c == 'J').count();
+
+        if self.part_2 {
+            card_counts.sort_unstable_by_key(|count| Reverse(*count));
+
+            if card_counts.is_empty() {
+                card_counts.push(jokers);
+            } else {
+                card_counts[0] += jokers;
+            }
+        } else {
+            if jokers > 0 {
+                card_counts.push(jokers);
+            }
+            card_counts.sort_unstable_by_key(|count| Reverse(*count));
+        }
+
         match card_counts.len() {
             1 => HandType::FiveOfAKind,
             2 => {
-                card_counts.sort_unstable_by_key(|(_, count)| Reverse(*count));
-
-                if card_counts[0].1 == 4 {
+                if card_counts[0] == 4 {
                     HandType::FourOfAKind
                 } else {
                     HandType::FullHouse
                 }
             }
             3 => {
-                card_counts.sort_unstable_by_key(|(_, count)| Reverse(*count));
-
-                if card_counts[0].1 == 3 {
+                if card_counts[0] == 3 {
                     HandType::ThreeOfAKind
                 } else {
                     HandType::TwoPair
@@ -124,7 +139,7 @@ impl Ord for Hand<'_> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         match self.hand_type().cmp(&other.hand_type()) {
             Equal => zip(self.cards.chars(), other.cards.chars())
-                .map(|(a, b)| card_value(a).cmp(&card_value(b)))
+                .map(|(a, b)| card_value(a, self.part_2).cmp(&card_value(b, self.part_2)))
                 .find(|cmp| *cmp != Equal)
                 .unwrap_or(Equal),
             ord => ord,
@@ -146,35 +161,75 @@ impl PartialEq for Hand<'_> {
 
 impl Eq for Hand<'_> {}
 
-fn card_value(card: char) -> u8 {
+fn card_value(card: char, part_2: bool) -> u8 {
     match card {
-        num @ '2'..='9' => (num as u32 - '2' as u32) as u8,
-        'T' => 8,
-        'J' => 9,
-        'Q' => 10,
-        'K' => 11,
-        'A' => 12,
+        num @ '2'..='9' => (num as u32 - '0' as u32) as u8,
+        'T' => 10,
+        'J' => {
+            if part_2 {
+                1
+            } else {
+                11
+            }
+        }
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
         _ => panic!("Invalid card"),
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::cmp::Ordering;
+
     use super::*;
 
     #[test]
-    fn test_hand_type() {
-        assert_eq!(Hand::new("AAAAA").hand_type(), HandType::FiveOfAKind);
-        assert_eq!(Hand::new("AA8AA").hand_type(), HandType::FourOfAKind);
-        assert_eq!(Hand::new("23332").hand_type(), HandType::FullHouse);
-        assert_eq!(Hand::new("TTT98").hand_type(), HandType::ThreeOfAKind);
-        assert_eq!(Hand::new("23432").hand_type(), HandType::TwoPair);
-        assert_eq!(Hand::new("A23A4").hand_type(), HandType::OnePair);
-        assert_eq!(Hand::new("23456").hand_type(), HandType::HighCard);
+    fn test_hand_type_part_1() {
+        assert_eq!(Hand::new("AAAAA", false).hand_type(), HandType::FiveOfAKind);
+        assert_eq!(Hand::new("AA8AA", false).hand_type(), HandType::FourOfAKind);
+        assert_eq!(Hand::new("23332", false).hand_type(), HandType::FullHouse);
+        assert_eq!(
+            Hand::new("TTT98", false).hand_type(),
+            HandType::ThreeOfAKind
+        );
+        assert_eq!(Hand::new("23432", false).hand_type(), HandType::TwoPair);
+        assert_eq!(Hand::new("A23A4", false).hand_type(), HandType::OnePair);
+        assert_eq!(Hand::new("23456", false).hand_type(), HandType::HighCard);
     }
 
     #[test]
-    fn test_hand_compare() {
-        assert_eq!(Hand::new("AAAAA").hand_type(), HandType::FiveOfAKind);
+    fn test_hand_type_part_2() {
+        assert_eq!(Hand::new("32T3K", true).hand_type(), HandType::OnePair);
+        assert_eq!(Hand::new("KK677", true).hand_type(), HandType::TwoPair);
+        assert_eq!(Hand::new("T55J5", true).hand_type(), HandType::FourOfAKind);
+        assert_eq!(Hand::new("KTJJT", true).hand_type(), HandType::FourOfAKind);
+        assert_eq!(Hand::new("QQQJA", true).hand_type(), HandType::FourOfAKind);
+        assert_eq!(Hand::new("JJJJJ", true).hand_type(), HandType::FiveOfAKind);
+    }
+
+    #[test]
+    fn test_card_value_part_2() {
+        assert_eq!(card_value('J', true), 1);
+        assert_eq!(card_value('2', true), 2);
+        assert_eq!(card_value('9', true), 9);
+        assert_eq!(card_value('T', true), 10);
+    }
+
+    #[test]
+    fn test_hand_cmp_part_2() {
+        assert_eq!(
+            Hand::new("22222", true).cmp(&Hand::new("JJJJJ", true)),
+            Ordering::Greater
+        );
+        assert_eq!(
+            Hand::new("KTJJT", true).cmp(&Hand::new("QQQJA", true)),
+            Ordering::Greater
+        );
+        assert_eq!(
+            Hand::new("QQQJA", true).cmp(&Hand::new("T55J5", true)),
+            Ordering::Greater
+        );
     }
 }
