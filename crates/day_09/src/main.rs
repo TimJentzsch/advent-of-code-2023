208a8ce7d8f5e62631fd7619146768200272e9e6
Tim Jentzsch <commits@timjen.net>
2023-12-09 18:21:11 +0100

    Implement day 09 part 1

--- crates/day_09/src/main.rs
+++ crates/day_09/src/main.rs
@@ -4,16 +4,55 @@ fn main() {
     Day09::run();
 }
 
+type Num = i32;
+
 struct Day09;
 
-impl AocDay<u32, u32> for Day09 {
+impl AocDay<Num, u32> for Day09 {
     const DAY: u8 = 9;
 
-    fn part_1(_input: &str) -> u32 {
-        todo!()
+    fn part_1(input: &str) -> Num {
+        input
+            .lines()
+            .map(|line| {
+                let sequence: Vec<_> = line
+                    .split_ascii_whitespace()
+                    .map(|num| num.parse::<Num>().expect("Invalid number"))
+                    .collect();
+
+                extrapolate_value(sequence)
+            })
+            .sum()
     }
 
     fn part_2(_input: &str) -> u32 {
         todo!()
+    }
+}
+
+fn extrapolate_value(sequence: Vec<Num>) -> Num {
+    if sequence.iter().all(|num| *num == 0) {
+        0
+    } else {
+        let next_sequence: Vec<_> = sequence
+            .windows(2)
+            .map(|window| window[1] - window[0])
+            .collect();
+        let next_diff = extrapolate_value(next_sequence);
+        sequence.last().unwrap() + next_diff
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+
+        assert_eq!(Day09::part_1(input), 114);
     }
 }
