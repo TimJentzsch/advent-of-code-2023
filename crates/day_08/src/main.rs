2dca689b3a2f7c0494e73b82e1698503a1bb02cd
Tim Jentzsch <commits@timjen.net>
2023-12-09 13:31:55 +0100

    Add day 08 part 2 naive solution
    Doesn't seem to terminate in an acceptable time

--- crates/day_08/src/main.rs
+++ crates/day_08/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
 struct Day08;
 
-impl AocDay<usize, ()> for Day08 {
+impl AocDay<usize, usize> for Day08 {
     const DAY: u8 = 8;
 
     fn part_1(input: &str) -> usize {
@@ -50,8 +50,43 @@ impl AocDay<usize, ()> for Day08 {
         steps
     }
 
-    fn part_2(_input: &str) {
-        todo!()
+    fn part_2(input: &str) -> usize {
+        let (instructions, rest) = input.split_once("\n\n").unwrap();
+
+        let map: HashMap<_, _> = rest
+            .lines()
+            .map(|line| {
+                let (node, rest) = line.split_once(" = ").unwrap();
+                let (left, right) = rest
+                    .split_once(", ")
+                    .map(|(left, right)| {
+                        (left.trim_start_matches('('), right.trim_end_matches(')'))
+                    })
+                    .unwrap();
+
+                (node, (left, right))
+            })
+            .collect();
+
+        let mut nodes: Vec<_> = map.keys().filter(|node| node.ends_with('A')).collect();
+        let mut steps = 0;
+        let mut instructions = instructions.chars().cycle();
+
+        while !nodes.iter().all(|node| node.ends_with('Z')) {
+            let paths = nodes.iter().map(|node| {
+                map.get(*node)
+                    .unwrap_or_else(|| panic!("Did not find node {}", node))
+            });
+
+            nodes = match instructions.next().unwrap() {
+                'L' => paths.map(|(left, _)| left).collect(),
+                'R' => paths.map(|(_, right)| right).collect(),
+                instruction => panic!("Invalid instruction {instruction}"),
+            };
+            steps += 1;
+        }
+
+        steps
     }
 }
 
@@ -80,5 +115,21 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
 
         assert_eq!(Day08::part_1(input), 6);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        assert_eq!(Day08::part_2(input), 6);
     }
 }
