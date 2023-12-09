0acaf10606cab16985dcb2f51c061afbade56dcd
Tim Jentzsch <commits@timjen.net>
2023-12-09 18:03:40 +0100

    Try to make day 08 part 2 solve in time
    It's still too slow

--- crates/day_08/src/main.rs
+++ crates/day_08/src/main.rs
@@ -13,22 +13,7 @@ impl AocDay<usize, usize> for Day08 {
     const DAY: u8 = 8;
 
     fn part_1(input: &str) -> usize {
-        let (instructions, rest) = input.split_once("\n\n").unwrap();
-
-        let map: HashMap<_, _> = rest
-            .lines()
-            .map(|line| {
-                let (node, rest) = line.split_once(" = ").unwrap();
-                let (left, right) = rest
-                    .split_once(", ")
-                    .map(|(left, right)| {
-                        (left.trim_start_matches('('), right.trim_end_matches(')'))
-                    })
-                    .unwrap();
-
-                (node, (left, right))
-            })
-            .collect();
+        let (instructions, map) = parse_map(input);
 
         let mut node = "AAA";
         let mut steps = 0;
@@ -51,43 +36,104 @@ impl AocDay<usize, usize> for Day08 {
     }
 
     fn part_2(input: &str) -> usize {
-        let (instructions, rest) = input.split_once("\n\n").unwrap();
+        let (instructions, map) = parse_map(input);
 
-        let map: HashMap<_, _> = rest
-            .lines()
-            .map(|line| {
-                let (node, rest) = line.split_once(" = ").unwrap();
-                let (left, right) = rest
-                    .split_once(", ")
-                    .map(|(left, right)| {
-                        (left.trim_start_matches('('), right.trim_end_matches(')'))
-                    })
-                    .unwrap();
+        const CACHED_REPETITIONS: usize = 100;
 
-                (node, (left, right))
+        let full_instructions: HashMap<_, _> = map
+            .keys()
+            .map(|node| {
+                let mut end_node_steps = Vec::new();
+                let mut cur_node = node;
+                let mut steps = 0;
+
+                if is_end_node(&cur_node) {
+                    end_node_steps.push(steps);
+                }
+
+                for instruction in instructions
+                    .chars()
+                    .cycle()
+                    .take(instructions.len() * CACHED_REPETITIONS)
+                {
+                    steps += 1;
+
+                    let (left, right) = map
+                        .get(cur_node)
+                        .unwrap_or_else(|| panic!("Did not find node {}", cur_node));
+
+                    cur_node = match instruction {
+                        'L' => left,
+                        'R' => right,
+                        instruction => panic!("Invalid instruction {instruction}"),
+                    };
+
+                    if is_end_node(&cur_node) {
+                        end_node_steps.push(steps);
+                    }
+                }
+
+                (node, (cur_node, end_node_steps))
             })
             .collect();
 
-        let mut nodes: Vec<_> = map.keys().filter(|node| node.ends_with('A')).collect();
-        let mut steps = 0;
-        let mut instructions = instructions.chars().cycle();
+        println!("Cache created");
 
-        while !nodes.iter().all(|node| node.ends_with('Z')) {
-            let paths = nodes.iter().map(|node| {
-                map.get(*node)
-                    .unwrap_or_else(|| panic!("Did not find node {}", node))
-            });
+        let mut nodes: Vec<_> = map.keys().filter(is_start_node).collect();
+        let mut total_steps = 0;
 
-            nodes = match instructions.next().unwrap() {
-                'L' => paths.map(|(left, _)| left).collect(),
-                'R' => paths.map(|(_, right)| right).collect(),
-                instruction => panic!("Invalid instruction {instruction}"),
-            };
-            steps += 1;
+        'search: loop {
+            let node_instructions: Vec<_> = nodes
+                .iter()
+                .map(|node| full_instructions.get(node).unwrap())
+                .collect();
+
+            let (_, end_node_steps) = node_instructions[0];
+
+            for steps in end_node_steps.iter() {
+                if node_instructions
+                    .iter()
+                    .all(|(_, end_steps_2)| end_steps_2.contains(steps))
+                {
+                    break 'search total_steps + steps;
+                }
+            }
+
+            nodes = node_instructions
+                .into_iter()
+                .map(|(next_node, _)| *next_node)
+                .collect();
+
+            total_steps += instructions.len() * CACHED_REPETITIONS;
         }
-
-        steps
     }
+}
+
+fn parse_map(input: &str) -> (&str, HashMap<&str, (&str, &str)>) {
+    let (instructions, rest) = input.split_once("\n\n").unwrap();
+
+    let map: HashMap<_, _> = rest
+        .lines()
+        .map(|line| {
+            let (node, rest) = line.split_once(" = ").unwrap();
+            let (left, right) = rest
+                .split_once(", ")
+                .map(|(left, right)| (left.trim_start_matches('('), right.trim_end_matches(')')))
+                .unwrap();
+
+            (node, (left, right))
+        })
+        .collect();
+
+    (instructions, map)
+}
+
+fn is_start_node(node: &&&str) -> bool {
+    node.ends_with('A')
+}
+
+fn is_end_node(node: &&&str) -> bool {
+    node.ends_with('Z')
 }
 
 #[cfg(test)]
