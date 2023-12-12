006a0b58399655062a8c241ba62764b9871abe36
Tim Jentzsch <commits@timjen.net>
2023-12-12 20:33:10 +0100

    Add automatic downloading of input

--- crates/aoc_utils/src/lib.rs
+++ crates/aoc_utils/src/lib.rs
@@ -3,10 +3,13 @@ use std::fmt::Display;
 use std::fs;
 use std::hint::black_box;
 use std::panic;
+use std::path::Path;
 use std::time::Duration;
 use std::time::Instant;
 
 use clap::Parser;
+use inquire::Confirm;
+use inquire::Text;
 
 #[derive(Parser, Debug)]
 #[command()]
@@ -44,16 +47,61 @@ pub trait AocDay<P1: Eq + Debug, P2: Eq + Debug> {
         format!("inputs/{}", Self::get_file_name())
     }
 
-    /// Try to get the input file for this day.
-    fn get_input() -> String {
+    /// Try to read the input from the file system.
+    fn get_input_from_file() -> Option<String> {
         let file_path = Self::get_file_path();
 
-        fs::read_to_string(file_path.clone())
-            .unwrap_or_else(|_| {
-                panic!("Failed to read input file, make sure to add it at {file_path}",)
-            })
-            .trim_end()
-            .to_string()
+        if Path::new(&file_path).exists() {
+            Some(
+                fs::read_to_string(file_path.clone())
+                    .unwrap()
+                    .trim_end()
+                    .to_string(),
+            )
+        } else {
+            None
+        }
+    }
+
+    /// Try to read the input from the file system.
+    fn download_and_save_input(session_cookie: &str) -> String {
+        let text = reqwest::blocking::Client::new()
+            .get(format!(
+                "https://adventofcode.com/2023/day/{}/input",
+                Self::DAY
+            ))
+            .header("Cookie", format!("session={session_cookie}"))
+            .send()
+            .unwrap()
+            .text()
+            .unwrap();
+
+        let file_path = Self::get_file_path();
+        fs::write(file_path, text.clone()).unwrap();
+
+        text
+    }
+
+    fn get_input() -> String {
+        if let Some(input) = Self::get_input_from_file() {
+            return input;
+        }
+
+        let file_path = Self::get_file_path();
+
+        eprintln!();
+
+        if !Confirm::new(&format!(
+            "The input file {file_path} does not exist. Do you want to download it?"
+        ))
+        .prompt()
+        .unwrap()
+        {
+            panic!("No input provided! You can add it at {file_path}.")
+        }
+
+        let session_cookie = get_session_cookie();
+        Self::download_and_save_input(&session_cookie)
     }
 
     fn run() {
@@ -137,4 +185,22 @@ fn run_part<T: Eq + Debug>(part: impl Fn(&str) -> T, input: &str, bench: bool) -
     }
 
     RunResult { res, times }
+}
+
+fn get_session_cookie() -> String {
+    let saved_cookie_path = Path::new("inputs/.session_cookie");
+
+    if saved_cookie_path.exists() {
+        fs::read_to_string("inputs/.session_cookie")
+            .unwrap()
+            .trim()
+            .to_string()
+    } else {
+        let session_cookie = Text::new("What is your Advent of Code session cookie?")
+        .with_help_message("You can get your session cookie on https://adventofcode.com using your browsers dev tools.").prompt().unwrap().trim().to_string();
+
+        fs::write(saved_cookie_path, session_cookie.clone()).unwrap();
+
+        session_cookie
+    }
 }
