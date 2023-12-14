818004a95eb265a87318ccd91ba97700a2d4872c
Tim Jentzsch <commits@timjen.net>
2023-12-14 21:45:44 +0100

    Add day 14 boilerplate

---
+++ crates/day_14/src/main.rs
@@ -0,0 +1,19 @@
+use aoc_utils::AocDay;
+
+fn main() {
+    Day14::run();
+}
+
+struct Day14;
+
+impl AocDay<usize, usize> for Day14 {
+    const DAY: u8 = 14;
+
+    fn part_1(_input: &str) -> usize {
+        todo!()
+    }
+
+    fn part_2(_input: &str) -> usize {
+        todo!()
+    }
+}
