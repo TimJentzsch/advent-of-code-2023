da1eacd60f7f70cc661e8b45cdb3f600b7e89645
Tim Jentzsch <commits@timjen.net>
2023-12-14 23:41:43 +0100

    Implement day 14 part 2

--- crates/day_14/src/main.rs
+++ crates/day_14/src/main.rs
@@ -16,8 +16,30 @@ impl<const SIZE: usize> AocDay<usize, usize> for Day14<SIZE> {
         Platform::<SIZE>::parse(input).tilt_north().total_load()
     }
 
-    fn part_2(_input: &str) -> usize {
-        todo!()
+    fn part_2(input: &str) -> usize {
+        const ITERATIONS: usize = 1_000_000_000;
+
+        let mut platform = Platform::<SIZE>::parse(input);
+        let mut cache = Vec::with_capacity(100_000);
+
+        for iter in 1..=ITERATIONS {
+            cache.push(platform.clone());
+            platform = platform.spin_cycle();
+
+            if let Some(cache_idx) = cache.iter().position(|a| a == &platform) {
+                let cycle_size = iter - cache_idx;
+                let remaining = ITERATIONS - iter;
+                let remaining_short = remaining % cycle_size;
+
+                for _ in 0..remaining_short {
+                    platform = platform.spin_cycle();
+                }
+
+                break;
+            }
+        }
+
+        platform.total_load()
     }
 }
 
@@ -39,5 +61,21 @@ O.#..O.#.#
 #OO..#....";
 
         assert_eq!(Day14::<10>::part_1(input), 136);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+
+        assert_eq!(Day14::<10>::part_2(input), 64);
     }
 }
--- crates/day_14/src/types.rs
+++ crates/day_14/src/types.rs
@@ -72,6 +72,10 @@ impl<const SIZE: usize> Platform<SIZE> {
             .sum()
     }
 
+    pub fn spin_cycle(self) -> Self {
+        self.tilt_north().tilt_west().tilt_south().tilt_east()
+    }
+
     pub fn tilt_north(mut self) -> Self {
         for col in 0..SIZE {
             let mut next_free_row = 0;
@@ -86,6 +90,76 @@ impl<const SIZE: usize> Platform<SIZE> {
                             debug_assert!(self.entries[next_free_row][col].is_none());
                             self.entries[next_free_row][col] = Some(rock);
                             next_free_row += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        self
+    }
+
+    pub fn tilt_south(mut self) -> Self {
+        for col in 0..SIZE {
+            let mut next_free_row = SIZE - 1;
+
+            for row in (0..SIZE).rev() {
+                if let Some(rock) = self.entries[row][col] {
+                    match rock {
+                        Rock::Cube => next_free_row = row.saturating_sub(1),
+                        Rock::Round => {
+                            self.entries[row][col] = None;
+
+                            debug_assert!(self.entries[next_free_row][col].is_none());
+                            self.entries[next_free_row][col] = Some(rock);
+
+                            next_free_row = next_free_row.saturating_sub(1);
+                        }
+                    }
+                }
+            }
+        }
+
+        self
+    }
+
+    pub fn tilt_west(mut self) -> Self {
+        for row in 0..SIZE {
+            let mut next_free_col = 0;
+
+            for col in 0..SIZE {
+                if let Some(rock) = self.entries[row][col] {
+                    match rock {
+                        Rock::Cube => next_free_col = col + 1,
+                        Rock::Round => {
+                            self.entries[row][col] = None;
+
+                            debug_assert!(self.entries[row][next_free_col].is_none());
+                            self.entries[row][next_free_col] = Some(rock);
+                            next_free_col += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        self
+    }
+
+    pub fn tilt_east(mut self) -> Self {
+        for row in 0..SIZE {
+            let mut next_free_col = SIZE - 1;
+
+            for col in (0..SIZE).rev() {
+                if let Some(rock) = self.entries[row][col] {
+                    match rock {
+                        Rock::Cube => next_free_col = col.saturating_sub(1),
+                        Rock::Round => {
+                            self.entries[row][col] = None;
+
+                            debug_assert!(self.entries[row][next_free_col].is_none());
+                            self.entries[row][next_free_col] = Some(rock);
+                            next_free_col = next_free_col.saturating_sub(1);
                         }
                     }
                 }
@@ -129,5 +203,75 @@ O..#.OO...
         );
 
         assert_eq!(platform.tilt_north(), tilted_platform);
+    }
+
+    #[test]
+    fn test_platform_spin_cycle() {
+        let mut platform = Platform::<10>::parse(
+            "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....",
+        );
+
+        // Cycle 1
+        platform = platform.spin_cycle();
+        assert_eq!(
+            platform,
+            Platform::<10>::parse(
+                ".....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....",
+            )
+        );
+
+        // Cycle 2
+        platform = platform.spin_cycle();
+        assert_eq!(
+            platform,
+            Platform::<10>::parse(
+                ".....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O",
+            )
+        );
+
+        // Cycle 3
+        platform = platform.spin_cycle();
+        assert_eq!(
+            platform,
+            Platform::<10>::parse(
+                ".....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O",
+            )
+        );
     }
 }
