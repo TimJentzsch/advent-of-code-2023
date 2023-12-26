0ad369f46293b46f386f2fe4edcb633057b8ed45
Tim Jentzsch <commits@timjen.net>
2023-12-26 11:38:44 +0100

    Add day 10 boilerplate

---
+++ days/day_10/src/main.rs
@@ -0,0 +1,19 @@
+use aoc_utils::AocDay;
+
+fn main() {
+    Day10::run();
+}
+
+struct Day10;
+
+impl AocDay<usize, usize> for Day10 {
+    const DAY: u8 = 10;
+
+    fn part_1(_input: &str) -> usize {
+        todo!()
+    }
+
+    fn part_2(_input: &str) -> usize {
+        todo!()
+    }
+}
